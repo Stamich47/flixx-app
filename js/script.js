e8071747bdcd15655c51bf21f9eda804e1d9a370
Stamich47 <159186195+Stamich47@users.noreply.github.com>
2024-05-16 14:24:10 -0700

    Create function to add popular movies to index

--- js/script.js
+++ js/script.js
@@ -2,6 +2,64 @@ const global = {
   currentPage: window.location.pathname,
 };
 
+async function displayPopularMovies() {
+  const { results } = await fetchAPIData('movie/popular');
+
+  results.forEach((movie) => {
+    const divCard = document.createElement('div');
+    divCard.classList.add('card');
+
+    const cardTitle = document.createElement('h5');
+    cardTitle.classList.add('card-title');
+    cardTitle.innerText = `${movie.title}`;
+
+    const cardText = document.createElement('p');
+    cardText.classList.add('card-text');
+    const releaseDate = document.createElement('small');
+    releaseDate.classList.add('text-muted');
+    releaseDate.appendChild(
+      document.createTextNode(`Release: ${movie.release_date}`)
+    );
+    cardText.appendChild(releaseDate);
+
+    const link = document.createElement('a');
+    link.setAttribute('href', `movie-details.html?id=${movie.id}`);
+
+    const img = document.createElement('img');
+    img.classList.add('card-img-top');
+    img.setAttribute('alt', `${movie.title}`);
+    img.setAttribute(
+      'src',
+      `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+    );
+    link.appendChild(img);
+
+    const CardBody = document.createElement('div');
+    CardBody.classList.add('card-body');
+    CardBody.appendChild(cardTitle);
+    CardBody.appendChild(cardText);
+
+    divCard.appendChild(link);
+    divCard.appendChild(CardBody);
+
+    document.getElementById('popular-movies').appendChild(divCard);
+  });
+}
+
+// Fetch data from TMDB API
+
+async function fetchAPIData(endpoint) {
+  const API_KEY = '967c935dc81716e47682ad65c5f49d3a';
+  const API_URL = 'https://api.themoviedb.org/3/';
+
+  const response = await fetch(
+    `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-us`
+  );
+  const data = await response.json();
+
+  return data;
+}
+
 // Highlight active link
 function highlightActiveLink() {
   const links = document.querySelectorAll('.nav-link');
@@ -17,7 +75,7 @@ function init() {
   switch (global.currentPage) {
     case '/':
     case '/index.html':
-      console.log('Home');
+      displayPopularMovies();
       break;
     case '/shows.html':
       console.log('TV Shows');
